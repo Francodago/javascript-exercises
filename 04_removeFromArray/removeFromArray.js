6ca2578e88774e08033ede992a5d38767183b21d
Franco D'Agostino <144281050+Francodago@users.noreply.github.com>
2025-04-22 16:24:29 -0400

    Finished the exercises till number 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(myArray, value) {
-    let filteredArray = myArray.filter((item)=> item !== value )
+const removeFromArray = function(myArray, ...values) {
+    let filteredArray = myArray.filter((item)=> !values.includes(item) );
     return filteredArray;
 };
+//removeFromArray([1, 2, 3, 4], 3, 2)
+//[1, 2, 3, 4], 7, "tacos")
+// Should return [1, 4]
+//If the item is included → we don't want it.
+//If the item is NOT included → we want to keep it.
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum=0;
+   // Check if either num1 or num2 is not a number or is negative
+   if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+   if (num1 < 0 || num2 < 0) return 'ERROR';
+   if (!Number.isInteger(num1)|| !Number.isInteger(num2)) return 'ERROR';
+  let start =Math.min(num1,num2);
+  let end =Math.max(num1,num2);
 
+  for(let i=start;i<=end;i++){
+    sum+=i
+  }
+  return sum;
 };
+//sumAll(1, 4); // 1 + 2 + 3 + 4 = 10
 
 // Do not edit below this line
 module.exports = sumAll;
