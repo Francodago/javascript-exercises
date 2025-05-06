2887a5f0256a357b09bc132b8b2e447132871a47
Franco D'Agostino <144281050+Francodago@users.noreply.github.com>
2025-05-06 18:20:44 -0400

    finished the second part of the exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+ // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+ let regEx = /[\W_]/g; //or let regEx = /[^A-Za-z0-9]/g;
+ let lowStr = str.toLowerCase().replace(regEx, "");
+ // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase()
+ // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+ // let lowStr = "amanaplanacanalpanama";
+ let reverseStr = lowStr.split("").reverse().join("");
+ // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+ // And, let reverseStr = "amanaplanacanalpanama";
 
+ // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+ return lowStr === reverseStr;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+// Use reduce method to reduce the array by comparing current age with previous age
+return array.reduce((oldest, currentPerson) => {
+    // oldestAge gets the age of the oldest person's year of death and birth
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
 
+    // currentAge gets the age of the current person's year of death and birth
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+    // return name if current age is older than the oldest age, else return current oldest age
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
-
+const getAge = function(birth, death) {
+    if (!death) {
+   death = new Date().getFullYear(); // return current year using Date()
+   }
+   return death - birth; // else just return age using death minus birth
 // Do not edit below this line
+}
 module.exports = findTheOldest;
